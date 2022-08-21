7059d7e7325340273887763a0657b7c27c1b7e9c
Elmir22 <elmirabbasov002@gmail.com>
2022-08-21 14:38:30 +0000

    app.js change

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
 "use strict";
 $(document).ready(function () {
 
+
+
+
+
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("input").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
+            async function input() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("input").val("")
+                console.log(data)
+            }
+            input()
+        }
+    })
+
+    $(".btn").click(function (e) {
+        e.preventDefault()
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("input").val()}&appid=012d3a58b4fd8ab58c50576d8d087fc1&units=metric`
+        async function input() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels_like)
+            $("input").val("")
+            console.log(data)
+        }
+        input()
+    })
+
+
+
+
+
 });
